05b2b76fda187aa33207182a16dabdee471172fb
Garrison Jensen <garrison.jensen@gmail.com>
2024-01-28 12:17:15 -0800

    Update script to get name of daily question

--- make_daily_file.rb
+++ make_daily_file.rb
@@ -1,11 +1,40 @@
+require 'net/http'
+require 'uri'
+require 'json'
 require 'date'
 
-current_date = Date.today.strftime('%d-%m-%Y')
+# Define the URL and the query
+url = URI.parse('https://leetcode.com/graphql')
+query = {
+  "query" => "\n query questionOfToday {\n activeDailyCodingChallengeQuestion {\n date\n userStatus\n link\n question {\n acRate\n difficulty\n freqBar\n frontendQuestionId: questionFrontendId\n isFavor\n paidOnly: isPaidOnly\n status\n title\n titleSlug\n hasVideoSolution\n hasSolution\n topicTags {\n name\n id\n slug\n }\n }\n }\n}\n ",
+  "variables" => {},
+  "operationName" => "questionOfToday"
+}
 
-arguments = ARGV.join(' ')
+# Create the HTTP request
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+request = Net::HTTP::Post.new(url.request_uri)
+request.content_type = 'application/json'
+request.body = query.to_json
 
-filename = "#{current_date} #{arguments}.rb"
+# Send the request and parse the response
+response = http.request(request)
+result = JSON.parse(response.body)
 
-puts filename
+# Extract and display the daily question name
+if result && result["data"] && result["data"]["activeDailyCodingChallengeQuestion"] && result["data"]["activeDailyCodingChallengeQuestion"]["question"]
+  daily_question = result["data"]["activeDailyCodingChallengeQuestion"]["question"]["title"]
+  id = result["data"]["activeDailyCodingChallengeQuestion"]["question"]["frontendQuestionId"]
 
-File.open(filename, 'w') {}
+  current_date = Date.today.strftime('%d-%m-%Y')
+
+  filename = "#{current_date} #{id}. #{daily_question}.rb"
+
+  puts filename
+
+  File.open(filename, 'w') {}
+else
+  puts "Unable to retrieve the daily question."
+end
+
