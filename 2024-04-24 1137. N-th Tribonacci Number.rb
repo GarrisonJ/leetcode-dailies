a67d3bdf46779a108a142d3f8959d6a5b33630b3
Garrison Jensen <garrison.jensen@gmail.com>
2024-04-24 13:22:12 -0700

    add even better 2024-04-24

--- 2024-04-24 1137. N-th Tribonacci Number.rb
+++ 2024-04-24 1137. N-th Tribonacci Number.rb
@@ -9,3 +9,16 @@ def tribonacci(n)
         tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
     end
 end
+
+# @param {Integer} n
+# @return {Integer}
+def tribonacci(n)
+    return 0 if n == 0
+    return 1 if n == 1
+    return 1 if n == 2
+    t0 = 0
+    t1 = 1
+    t2 = 1
+    (n-2).times { t0, t1, t2 = t1, t2, t0 + t1 + t2 }
+    t2
+end
